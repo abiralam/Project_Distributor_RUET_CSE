7d10da78eaa6112aeec977f698fee851b0640d6d
abiralam <abir.alam051@gmail.com>
2022-10-08 01:06:45 +0600

    commit

--- dp/settings.py
+++ dp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$w687udf(a7#ak@tc!(w=2@%ckc5@f=+qa)vc$nqsi_zkb@ty1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','projectdistributor.herokuapp.com']
 
 
 # Application definition
