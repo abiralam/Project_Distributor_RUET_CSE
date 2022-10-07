4c465ba62070bbc356cd272e06b2b628f6343009
abiralam <abir.alam051@gmail.com>
2022-10-07 14:19:36 +0600

    commit

--- dp/urls.py
+++ dp/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from os import stat
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('', include('sort_cgpa.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
+
